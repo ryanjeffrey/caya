5c2799d9818c303460da478fbc8bc7ad6e198ca5
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-04-08 15:13:27 -0500

    Adding links

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+
+export default function About(){
+    return <div>
+        <h1>About</h1>
+        <Link to="/">Home Page</Link>
+    </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
+import {Link} from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>CAYA</h1>
+    <Link to ="/about">About</Link>
+    </div>
 }
