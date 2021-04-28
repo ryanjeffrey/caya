88c78404eb4e97d59c7817a487183a339e230b67
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-04-27 20:25:01 -0500

    Styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import pt from "../assets/images/pt-logo.png"
+import tfbg from "../assets/images/tfbg-logo.png"
 
 export default function Footer() {
     return (
       <div>
+        <div>
+        <img class="footer-img" src={pt} alt="psychology today logo" />
+        <img class="footer-img"src={tfbg} alt="therapy for black girls logo" />
+        </div>
         <footer>&copy; {new Date().getFullYear()} Caya Wellness LLC</footer>
       </div>
     )
