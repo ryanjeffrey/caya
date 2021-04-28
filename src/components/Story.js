a9f4a0e88dd500e469317010fe377503a5ffcc33
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-04-27 19:49:03 -0500

    Added story section

---
+++ src/components/Story.js
@@ -0,0 +1,88 @@
+import React from "react"
+import styled from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+
+const ImageBackground = styled(BackgroundImage)`
+  background-position: bottom 20% center;
+  background-size: cover;
+  height: 100%;
+
+  /* override the default margin for sibling elements  */
+  + * {
+    margin-top: 0;
+  }
+`
+
+const TextBox = styled("div")`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: flex-end;
+  width: 100%;
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p,
+  a {
+    color: #eeb462;
+    font-size: 2rem;
+    line-height: 1.2;
+  }
+
+  a {
+    margin-top: 0.5rem;
+  }
+`
+
+const Story = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "wave.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
+      <TextBox>
+        <div className="description">
+          <p>It is possible to change your life at any time.</p>
+          <br />
+          <br />
+          <p>
+            Sometimes we feel like we lack the tools or the hope to see a
+            positive change.
+          </p>
+          <br />
+          <br />
+          <p>
+            Whether you're struggling in the midst of trauma, chronic pain, or
+            depression- you are not alone. And you don't have to be strong all
+            of the time.
+          </p>
+          <br />
+          <br />
+          <p> We want to be with you in the darkness.</p>
+          <br />
+          <br />
+          <p>
+            {" "}
+            All we ask is for you to <em>come as you are.</em>
+          </p>
+          <button to="/about">Find Out More &gt;</button>
+        </div>
+      </TextBox>
+    </ImageBackground>
+  )
+}
+
+export default Story
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
-import { StaticImage } from "gatsby-plugin-image"
+import Story from "../components/Story"
 
 
 const Home = () => {
@@ -11,12 +11,8 @@ const Home = () => {
       <div className="description">
         <h4>CAYA</h4>
         <h5>come as you are</h5>
-        <br/>
-        <p>It is possible to change your life at any time.</p>
-        <p>Sometimes we feel like we lack the tools or the hope to see a positive change.</p>
-          <p>Whether you're struggling in the midst of trauma, chronic pain, or depression- you are not alone. And you don't have to be strong all of the time.</p><p> We want to be with you in the darkness.</p><p> All you must do is come as you are.</p>
       </div>
-      <button>Find Out More</button>
+      <Story />
     </Layout>
   )
 }
