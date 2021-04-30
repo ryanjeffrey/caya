a93c815841f31db10fe4821fb39981b4e097c9c9
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-04-30 15:15:00 -0500

    Styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import {Link} from 'gatsby'
 
 const Error = () => {
     return (
         <Layout>
-            <h1>Error Page</h1>
+            <h1>Hmm, that doesn't exist.</h1>
+            <p>No Worries.</p>
+            <Link to="/">Go Home</Link>
         </Layout>
     )
 }
