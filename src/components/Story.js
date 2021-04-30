2c36d1680416d012d9e8be555bdae414f2e3bd86
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-04-30 12:58:20 -0500

    Migrated story to gatsby-plugin-image

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,84 +1,54 @@
 import React from "react"
-import styled from "styled-components"
-import { Link, graphql, useStaticQuery } from "gatsby"
-import BackgroundImage from "gatsby-background-image"
-
-const ImageBackground = styled(BackgroundImage)`
-  background-position: bottom 20% center;
-  background-size: cover;
-  height: 100%;
-
-  /* override the default margin for sibling elements  */
-  + * {
-    margin-top: 0;
-  }
-`
-
-const TextBox = styled("div")`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: flex-end;
-  width: 100%;
-
-  h1 {
-    text-shadow: 1px 1px 3px #eeddff66;
-    font-size: 2.25rem;
-  }
-
-  p {
-    color: #eeb462;
-    font-size: 2rem;
-    line-height: 1.6;
-  }
-`
+import { StaticImage } from "gatsby-plugin-image"
+import {Link} from 'gatsby'
 
 const Story = () => {
-  const { image } = useStaticQuery(graphql`
-    query {
-      image: file(relativePath: { eq: "wave.jpg" }) {
-        sharp: childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
-    }
-  `)
-
   return (
-    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
-      <TextBox>
-        <div className="description">
-          <p>It is possible to change your life at any time.</p>
-          <br />
-          <br />
-          <p>
-            Sometimes we feel like we lack the tools or the hope to see a
-            positive change.
-          </p>
-          <br />
-          <br />
-          <p>
-            Whether you're struggling in the midst of trauma, chronic pain, or
-            depression- you are not alone. And you don't have to be strong all
-            of the time.
-          </p>
-          <br />
-          <br />
-          <p> We want to be with you in the darkness.</p>
-          <br />
-          <br />
-          <p>
-            {" "}
-            All we ask is that you <em>come as you are.</em>
-          </p>
-          <Link to="/services" className="btn">
-            Learn More &gt;
-          </Link>
-        </div>
-      </TextBox>
-    </ImageBackground>
+    <div style={{ display: "grid" }}>
+      {/* You can use a GatsbyImage component if the image is dynamic */}
+      <StaticImage
+        className="story-img"
+        style={{
+          gridArea: "1/1",
+        }}
+        layout="fullWidth"
+        // This is a presentational image, so the alt should be an empty string
+        alt=""
+        src={"../assets/images/wave.jpg"}
+        placeholder="blurred"
+        formats={["auto", "webp", "avif"]}
+      />
+      <div
+        className="story-text"
+        style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+        }}
+      >
+        {/* Any content here will be centered in the component */}
+        <p>It is possible to change your life at any time.</p>
+        <p>
+          Sometimes we feel like we lack the tools or the hope to see a positive
+          change.
+        </p>
+        <p>
+          Whether you're struggling in the midst of trauma, chronic pain, or
+          depression- you are not alone. And you don't have to be strong all of
+          the time.
+        </p>
+        <p> We want to be with you in the darkness.</p>
+        <p>
+          All we ask is that you <em>come as you are.</em>
+        </p>
+        <Link to="/services" className="btn">
+          Learn More &gt;
+        </Link>
+      </div>
+    </div>
   )
 }
 
