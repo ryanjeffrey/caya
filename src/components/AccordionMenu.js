e7aa363a171b2f4664f8a3588b483365aedaccf6
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-05-04 19:04:45 -0500

    Styled accordion

--- src/components/AccordionMenu.js
+++ src/components/AccordionMenu.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Accordion from "../components/Accordion"
+import { VscFeedback, VscChecklist } from "react-icons/vsc"
+import { FiTrendingUp } from "react-icons/fi"
 
 function AccordionMenu() {
   return (
@@ -7,7 +9,8 @@ function AccordionMenu() {
       <Accordion
         title="Psychotherapy"
         content="<p>Psychotherapy helps individuals manage a wide range of mental health-related concerns and diagnoses. CAYA wellness offers evidence-based, effective treatments for mental health conditions, coping with chronic illness, and healing past traumas. All psychotherapy patients will receive an initial psychological assessment for diagnosis and treatment planning.</p>
-        <h3>Treatments Offered:</h3>
+        </br>
+        <span><h3>Treatments Offered:</h3>
         <ul>
             <li>Symptom-Focused Hypnosis</li>
             <li>Cognitive Behavioral Therapy (CBT)</li>
@@ -16,16 +19,19 @@ function AccordionMenu() {
             <li>Prolonged Exposure (PE)</li>
             <li>Humanistic-Existential Psychotherapy</li>
         </ul>
+        </br>
         <h3>Fees:</h3>
         <ul>
             <li>Initial Intake: $225/per 55-minute session</li>
             <li>Psychotherapy: $185/per 50-minute session</li>
-        </ul>"
+        </ul>
+        </span>"
       />
       <Accordion
         title="Assessment"
         content="<p>Pre-surgical psychological assessment/clearance is often a requirement prior to specific surgeries and procedures. CAYA Wellness offers high-quality, compassionate, and straightforward presurgical assessments for patients seeking bariatric surgery, spinal cord stimulators, intrathecal pain pumps, and other surgical procedures.</p>
-        <h3>Fees*:</h3>
+        </br>
+        <span><h3>Fees*:</h3>
         <ul>
             <li>Bariatric Assessment: $500</li>
             <li>Spinal Cord Stimulator: $500</li>
@@ -35,21 +41,34 @@ function AccordionMenu() {
             <li>Other Assessment: $500-$900</li>
             <li>Spinal Cord Stimulator: $500</li>
             <p>*Includes assessment and testing services</p>
-        </ul>"
+        </ul>
+        </span>"
       />
       <Accordion
         title="Consultation"
         content="
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>CAYA Wellness provides a wide range of consultation services to large corporations and start-ups. Services include diversity and inclusion seminars, company wellness trainings, corporate assessment and intergroup relations.</p>
         </br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        "
+        <span>
+        <h3>Fee:</h3>
+        <p>$185/per hour</p>
+        </span>"
       />
       <Accordion
-        title="Coaching"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        title="Modern Love Coaching"
+        content="<p>CAYA Wellness offers coaching services for online daters that are in the process of finding lasting love. Techniques utilized in these services incorporate scientific research to help daters engage in best practices when it comes to dating. In addition, services help daters get clear on the traits of their ideal partner, navigate online dating platforms, and learn about common pitfalls and barriers to successful dating.</p>
+        </br>
+        <p>
+        **Differences between coaching and psychotherapy: While overlaps exist between coaching and psychotherapy, coaching and psychotherapy are two distinct practices. Psychotherapy focuses on the assessment and treatment of mental health disorders. Coaching is a future-focused approach that centers on achieving a particular goal. Coaching gives mentally healthy individuals tools to work towards their desired goal and is not a substitute for psychotherapy.</p>
+        </br>
+        <span><h3>Fee:</h3>
+        <p>$150/per 60-minute meeting</p>
+        </>"
+      />
+      <Accordion
+        title="Speaking &#38; Workshops"
+        content="<p>Please Contact For Rates</p>
+        "
       />
     </div>
   )
