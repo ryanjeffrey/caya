7ea074749dba72fb62c9d921254b353b456861ab
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-05-06 16:04:26 -0500

    Added photos

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
 import ContactForm from "../components/ContactForm"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Contact = () => {
   return (
     <Layout>
+      <StaticImage
+        src="../assets/images/calm.jpg"
+        alt=""
+        placeholder="blurred"
+        formats={["auto", "webp", "avif"]}
+      />
       <ContactForm />
     </Layout>
   )
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,26 +2,38 @@ import React from "react"
 import Layout from "../components/Layout"
 import {Link} from 'gatsby'
 import AccordionMenu from "../components/AccordionMenu"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 
 const Services = () => {
   return (
     <Layout>
-      <AccordionMenu />
+      <div className="smile">
+          <StaticImage
+            src="../assets/images/smile.jpg"
+            alt=""
+            placeholder="blurred"
+            formats={["auto", "webp", "avif"]}
+          />
+        </div>
+        <AccordionMenu />
       <div className="services-wrapper">
         <div className="btn-wrapper">
           <Link to="/contact" className="btn">
             Contact
           </Link>
         </div>
+        
         <span>
           <p>
-            <strong>Regarding Insurance: </strong>We are an out-of-network provider and
-            currently do not accept insurance. However, some insurance plans
-            offer out-of-network coverage, which may provide reimbursement for
-            mental health services. If your insurance provider allows for
-            out-of-network coverage, documentation and statements will be
-            provided for submission to your insurance company, upon request.
+            <strong>Regarding Insurance: </strong>We are an out-of-network
+            provider and currently do not accept insurance. However, some
+            insurance plans offer out-of-network coverage, which may provide
+            reimbursement for mental health services. If your insurance provider
+            allows for out-of-network coverage, documentation and statements
+            will be provided for submission to your insurance company, upon
+            request.
           </p>
         </span>
       </div>
